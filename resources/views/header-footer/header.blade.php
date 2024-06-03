1d4055b24e6f2fda56048cf9f1cf1597c57f1e98
AshikSazal <sojolislm@gmail.com>
2024-06-04 01:14:02 +0600

    fixing the header

--- resources/views/header-footer/header.blade.php
+++ resources/views/header-footer/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="grid grid-cols-3 p-4">
+    <nav class="grid grid-cols-3 p-4 shadow">
         <div class="flex items-center"><img src="./images/logo.png" alt="" height="150" width="150"></div>
         <div class="grid grid-cols-4 gap-2 items-center">
             <div class="text-2xl hover:text-nav_color hover:underline"><a href="#">Home</a></div>
@@ -8,11 +8,18 @@
             <div class="text-2xl hover:text-nav_color hover:underline"><a href="#">Contact</a></div>
         </div>
         <div class="grid grid-cols-2">
-            <div class="flex items-center justify-center relative">
-                <a href=""><img src="./images/cart.png" alt="" height="50" width="50"></a>
-                <span class="absolute text-2xl text-[#bb3d95] top-0 ml-14">0</span>
+            <div class="flex items-center justify-end relative">
+                <a href=""><img src="./images/cart.png" alt="" height="60" width="60"></a>
+                <span class="absolute text-2xl text-red-600 top-0 ml-14">0</span>
             </div>
-            <div class="flex items-center"><a href="./login">Signin</a></div>
+            <div class="flex items-center justify-center">
+                <a href="./login" class="flex items-center text-white bg-nav_color px-8 py-3 space-x-3 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-6">
+                        <path fill="currentColor" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+                    </svg>
+                    <span class="text-lg">Login</span>
+                </a>
+            </div>
         </div>
     </nav>
     <hr class="border-0 h-px bg-slate-300">
