0b62d41356fc3020a188a6609643e2a985c7b981
AshikSazal <sojolislm@gmail.com>
2024-06-06 17:48:04 +0600

    fix css

--- resources/views/header-footer/header.blade.php
+++ resources/views/header-footer/header.blade.php
@@ -47,24 +47,35 @@
             </div>            
         </div>
         <div class="md:hidden col-span-1 flex justify-center md:justify-end items-center">
-            <button >
+            <button id="mobileMenuButton" onclick="showMenu()">
                 <svg height="60" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
             </button>
         </div>
     </nav>
     <hr class="border-0 h-px bg-slate-300">
+    <div id="animatedList" class="navbar-list hidden">
+        <a href="#" class="hidden">Home</a>
+        <a href="#" class="hidden">Brand</a>
+        <a href="#" class="hidden">About</a>
+        <a href="#" class="hidden">Contact</a>
+    </div>        
 </header>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        console.log('Header loaded');
+    function showMenu() {
+    console.log("showMenu function executed");
+    var animatedList = document.getElementById('animatedList');
+    console.log("animatedList:", animatedList);
+    var links = animatedList.querySelectorAll('a');
+    console.log("links:", links);
 
-        // Example: Adding an event listener to the 'Home' link
-        document.querySelector('a[href="#"]').addEventListener('click', function(e) {
-            e.preventDefault();
-            alert('Home link clicked');
-        });
-
-        // Additional JavaScript code can be added here
+    links.forEach(function(link, index) {
+        setTimeout(function() {
+            link.classList.add('fadeIn');
+        }, index * 500);
     });
+
+    animatedList.classList.toggle('hidden');
+}
+
 </script>
