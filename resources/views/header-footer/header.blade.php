8357a405c918328a37bb7e0468c38804c3048bdf
AshikSazal <sojolislm@gmail.com>
2024-06-07 21:48:26 +0600

    fix css in header file

--- resources/views/header-footer/header.blade.php
+++ resources/views/header-footer/header.blade.php
@@ -35,24 +35,25 @@
             </div>            
         </div>
         <div class="md:hidden col-span-1 flex justify-center md:justify-end items-center">
-            <button class="list-header">
-                <svg height="60" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            <button class="list-header text-4xl">
+                {{-- <svg height="60" width="60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg> --}}
+                &#9776;
             </button>
         </div>
     </nav>
     <hr class="border-0 h-px bg-slate-300">
-    <div class="navbar-list rounded-md">
+    <div class="navbar-list bg-nav_color rounded-lg">
         <ul class="box-list">
-            <li class="bg-nav_color">
+            <li>
                 @include('components.landing-page.nav', ['text' => 'Home', 'href' => '#','flag'=>2])
             </li>
-            <li class="bg-nav_color">
+            <li>
                 @include('components.landing-page.nav', ['text' => 'Brand', 'href' => '#','flag'=>2])
             </li>
-            <li class="bg-nav_color">
+            <li>
                 @include('components.landing-page.nav', ['text' => 'About', 'href' => '#','flag'=>2])
             </li>
-            <li class="bg-nav_color">
+            <li>
                 @include('components.landing-page.nav', ['text' => 'Contact', 'href' => '#','flag'=>2])
             </li>
         </ul>
