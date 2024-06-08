91fcd08be615292021611c99b0cbd71176ca9cea
AshikSazal <sojolislm@gmail.com>
2024-06-08 22:34:40 +0600

    fixing the navbar

--- resources/views/header-footer/header.blade.php
+++ resources/views/header-footer/header.blade.php
@@ -36,7 +36,7 @@
     <div class="navbar-list absolute flex justify-center items-center">
         <ul class="box-list">
             <li class="nav-close flex justify-end">
-                <button class="border-2 border-nav_color rounded-full mb-2"><svg height="40" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></button>
+                <button class="border-4 border-nav_color rounded-full mb-2"><svg height="40" width="40" class="text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></button>
             </li>
             <li class="bg-nav_color">
                 @include('components.landing-page.nav', ['text' => 'Home', 'href' => '#','flag'=>2])
@@ -49,6 +49,15 @@
             </li>
             <li class="bg-nav_color">
                 @include('components.landing-page.nav', ['text' => 'Contact', 'href' => '#','flag'=>2])
+            </li>
+            <li>
+                <a href="./login" class="relative mt-4 flex items-center justify-center text-nav_color bg-white border-2 border-nav_color py-3 space-x-3 rounded-full overflow-hidden group">
+                    <span class="absolute inset-0 bg-nav_color transform scale-x-0 group-hover:scale-x-100 transition-transform duration-500 ease-in-out origin-center"></span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-6 relative z-10 transition-colors duration-500 ease-in-out group-hover:text-white">
+                        <path fill="currentColor" d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+                    </svg>
+                    <span class="relative z-10 text-lg transition-colors duration-500 ease-in-out group-hover:text-white">Login</span>
+                </a>
             </li>
         </ul>
     </div>        
@@ -124,7 +133,7 @@
             if(e.target==navList){
                 document.body.style.overflow = '';
                 document.body.style.background = '';
-                
+
                 // This is for animation
                 /*listItems.forEach(function(item, index) {
                     const reverseIndex = listItems.length - index - 1;
