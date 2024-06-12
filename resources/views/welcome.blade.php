f80bf542bc4ab235934b0f34cae3099e4ce30856
AshikSazal <sojolislm@gmail.com>
2024-06-12 16:15:25 +0600

    adding chatting system

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="./images/logo.png" type="image/png">
-    @vite(['resources/css/app.css','resources/css/header.css'])
+    @vite(['resources/css/app.css','resources/css/header.css','resources/css/chat.css'])
     <title>Bikey</title>
 </head>
 <body>
     @include('header-footer.header')
+    @include('features.chat.chat')
     <div class="bg-red-600">
         hello world
     </div>
