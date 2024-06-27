84cbf8c47b1876a6035c003b771eb43b183af5da
Ashik Sazal <sojolislm@gmail.com>
2024-06-28 01:52:04 +0600

    fixing border

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 <div class="p-2">
-    <input type="{{$type}}" name="{{$name}}" placeholder="{{$placeholder}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-sky_blue_color focus:border-sky_blue_color block w-full p-2.5 dark:bg-gray-700 dark:border-sky_blue_color dark:placeholder-gray-400 dark:text-white dark:focus:sky_blue_color dark:focus:border-sky_blue_color" required>
+    <input type="{{$type}}" name="{{$name}}" placeholder="{{$placeholder}}" class="rounded-full focus:outline-none block w-full p-2.5" required>
 </div>
