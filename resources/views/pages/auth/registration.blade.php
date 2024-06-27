8c11cb22dea9a445e00d40f6af3b7021dedb8e67
AshikSazal <sojolislm@gmail.com>
2024-06-27 17:25:20 +0600

    Adding Login and Registration facility

--- resources/views/pages/auth/registration.blade.php
+++ resources/views/pages/auth/registration.blade.php
@@ -5,6 +5,8 @@
     <div class="w-full flex justify-center items-center h-full">
         <x-card class="bg-sky_blue_color">
             <form>
+                <x-input type="text" name="name" placeholder="Enter Your Name" />
+                <x-input type="text" name="phone" placeholder="Enter Your Phone Number" />
                 <x-input type="text" name="email" placeholder="Enter Your E-Mail" />
                 <x-input type="password" name="password" placeholder="Enter Your Password" />
                 <div class="grid justify-center items-center">
