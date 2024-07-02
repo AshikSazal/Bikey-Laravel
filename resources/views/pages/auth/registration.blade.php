6e3cd0e47dc2dd93efebde467eb0a242be80db3b
Ashik Sazal <sojolislm@gmail.com>
2024-07-03 00:01:53 +0600

    Adding javascript for OTP input field

--- resources/views/pages/auth/registration.blade.php
+++ resources/views/pages/auth/registration.blade.php
@@ -85,9 +85,47 @@
             popUp.style.left = '0';
             popUp.style.width = '100%';
             popUp.style.height = '100%';
+
+            // OTP input
+            const inputs = document.querySelectorAll(".input-field input");
+            const button = document.getElementById("show-pop-up");
+            inputs.forEach((input, index1) => {
+                input.addEventListener("keyup",(e)=>{
+                    const currentInput = input;
+                    const nextInput = input.nextElementSibling;
+                    const prevInput = input.previousElementSibling;
+    
+                    if(currentInput.value.length>1){
+                        currentInput.value = "";
+                        return;
+                    }
+                    if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+                        nextInput.removeAttribute("disabled");
+                        nextInput.focus();
+                    }
+                    if(e.key === "Backspace"){
+                        inputs.forEach((input, index2)=>{
+                            if(index1 <= index2 && prevInput){
+                                input.setAttribute("disabled",true);
+                                input.value="";
+                                prevInput.focus();
+                            }
+                        })
+                    }
+                    if (!inputs[3].disabled && inputs[3].value !== "") {
+                        button.classList.add("active");
+                        return;
+                    }
+                    button.classList.remove("active");
+                });
+            });
+            window.addEventListener("load", () => inputs[0].focus());
         });
+        
     });
 
+
 </script>
+
 
 @endsection
