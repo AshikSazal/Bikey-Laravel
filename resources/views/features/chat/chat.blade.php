1b2b50c43f22e32f20241ac273d485a6de78854b
AshikSazal <sojolislm@gmail.com>
2024-08-04 01:51:57 +0600

    fixing the css of messaging

--- resources/views/features/chat/chat.blade.php
+++ resources/views/features/chat/chat.blade.php
@@ -47,7 +47,12 @@
         </div>
         <div style="scrollbar-width: none;" class="">
             <div class="mb-4" id="show-message">
-                
+                <div class="flex items-center">
+                    <span class="bg-sky_blue_color rounded-full p-2">Hello World</span>
+                    <svg height="25" width="25" class="text-orange_color -ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
+                        <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
+                    </svg>
+                </div>
             </div>
         </div>
         <div class="absolute bottom-0 w-full shadow-inner shadow-gray-200 rounded-b-lg">
