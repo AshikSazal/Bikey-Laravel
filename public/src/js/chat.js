fcb2ae3c0cb5277edd6cc73223eb130b72638b51
AshikSazal <sojolislm@gmail.com>
2024-08-14 02:45:34 +0600

    fix the chat issue

--- public/src/js/chat.js
+++ public/src/js/chat.js
@@ -1,9 +1,7 @@
 // scroll div to the bottom
 function scrollChat(){
-    console.log($("#show-message").offset().top);
-    console.log($("#show-message")[0].scrollHeight);
-    $("#show-message").animate({
-        scrollTop: $("#show-message").offset().top + $("#show-message")[0].scrollHeight
+    $("#show-message").parent().animate({
+        scrollTop: $("#show-message").parent().offset().top + $("#show-message").parent()[0].scrollHeight
     },0);
 }
 
@@ -31,19 +29,20 @@ $(document).ready(function(){
                 const chats = res.data;
                 let html='';
                 for(let i=0;i<chats.length;i++){
-                    let svg = '', textColor = 'text-black', background="#efefef", justify='';
+                    let svg = '', textColor = 'text-black', background="#efefef", justify='', margin="margin-right: 20px;";
                     if(chats[i].sender_id == sender_id){
                         background = '#1ca3e4';
                         textColor = 'text-white';
                         justify = 'justify-end';
+                        margin="margin-left: 20px;"
                         svg=`<svg height="20" width="20" class="text-sky_blue_color -ml-2 cursor-pointer hidden show-modify-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-id="${chats[i].id}">
                                 <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
                             </svg>`;
                     }
                     html += `
-                        <div class="flex items-center ${justify} show-modify mb-2" id="${chats[i].id}-chat">
+                        <div style="${margin}" class="flex items-center ${justify} show-modify mb-2" id="${chats[i].id}-chat">
                             ${svg}
-                            <span style="background: ${background};" class="${textColor} rounded-md px-3 py-2">${chats[i].message}</span>
+                            <span style="background: ${background};" class="${textColor} text-justify rounded-md px-3 py-2">${chats[i].message}</span>
                         </div>
                     `;
                 }
@@ -84,13 +83,14 @@ $(document).ready(function(){
                 message: message
             },
             success: function(res){
+                $("#message-input").val("");
                 const chat = res.chat;
                 const html = `
-                    <div class="flex items-center justify-end show-modify mb-2">
+                    <div style="margin-left: 20px;" class="flex items-center justify-end show-modify mb-2" id="${chat.id}-chat">
                         <svg height="20" width="20" class="text-sky_blue_color -ml-2 cursor-pointer show-modify-icon hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-id="26">
                             <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"></path>
                         </svg>
-                        <span style="background: #1ca3e4;" class="text-white rounded-md px-3 py-2">${chat.message}</span>
+                        <span style="background: #1ca3e4;" class="text-white text-justify rounded-md px-3 py-2">${chat.message}</span>
                     </div>
                 `;
                 $("#show-message").append(html);
@@ -115,13 +115,10 @@ Echo.private('broadcast-message')
 .listen('MessageSentEvent',(data)=>{
     if(sender_id == data.message.receiver_id && receiver_id == data.message.sender_id){
         const html = `
-                    <div class="flex items-center">
-                        <span class="bg-sky_blue_color rounded-full p-2">${data.message.message}</span>
-                        <svg height="25" width="25" class="text-orange_color -ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
-                            <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
-                        </svg>
-                    </div>
-                `;
+            <div style="margin-right: 20px;" class="flex items-center"  id="${data.message.id}-chat">
+                <span style="background: #efefef;" class="text-black text-justify rounded-md px-3 py-2">${data.message.message}</span>
+            </div>
+        `;
         $("#show-message").append(html);
         scrollChat();
         showModification();
