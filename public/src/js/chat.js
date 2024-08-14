7228741d0b93e7887abc4d3aa625bd38b5d524db
AshikSazal <sojolislm@gmail.com>
2024-08-14 17:16:53 +0600

    fixing css in chat.js file

--- public/src/js/chat.js
+++ public/src/js/chat.js
@@ -29,12 +29,12 @@ $(document).ready(function(){
                 const chats = res.data;
                 let html='';
                 for(let i=0;i<chats.length;i++){
-                    let svg = '', textColor = 'text-black', background="#efefef", justify='', margin="margin-right: 20px;";
+                    let svg = '', textColor = 'text-black', background="#e5e7eb", justify='', margin="margin-right: 20px;";
                     if(chats[i].sender_id == sender_id){
                         background = '#1ca3e4';
                         textColor = 'text-white';
                         justify = 'justify-end';
-                        margin="margin-left: 20px;"
+                        margin="margin-left: 30px;"
                         svg=`<svg height="20" width="20" class="text-sky_blue_color -ml-2 cursor-pointer hidden show-modify-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-id="${chats[i].id}">
                                 <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
                             </svg>`;
@@ -43,12 +43,28 @@ $(document).ready(function(){
                         <div style="${margin}" class="flex items-center ${justify} show-modify mb-2" id="${chats[i].id}-chat">
                             ${svg}
                             <span style="background: ${background};" class="${textColor} text-justify rounded-md px-3 py-2">${chats[i].message}</span>
+                            <div class="chat-modify-menu absolute right-0 top-full mt-2 w-32 bg-white border border-gray-300 rounded-lg shadow-lg hidden" data-id="${chats[i].id}">
+                                <ul class="list-none p-2">
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Delete</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     `;
                 }
                 $("#show-message").append(html);
                 scrollChat();
                 showModification();
+                $(".show-modify-icon").on('click', function(event) {
+                    console.log("Hello");
+                    event.stopPropagation(); // Prevent the click event from bubbling up to the document
+                    let chatId = $(this).data('id');
+                    $(`.chat-modify-menu[data-id="${chatId}"]`).toggleClass('hidden');
+                });
             },
             error: function(xhr, status, error){
                 const showError = document.getElementById('open-pop-up');
@@ -90,7 +106,7 @@ $(document).ready(function(){
                         <svg height="20" width="20" class="text-sky_blue_color -ml-2 cursor-pointer show-modify-icon hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-id="26">
                             <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"></path>
                         </svg>
-                        <span style="background: #1ca3e4;" class="text-white text-justify rounded-md px-3 py-2">${chat.message}</span>
+                        <span style="background: #e5e7eb;" class="text-white text-justify rounded-md px-3 py-2">${chat.message}</span>
                     </div>
                 `;
                 $("#show-message").append(html);
@@ -116,7 +132,7 @@ Echo.private('broadcast-message')
     if(sender_id == data.message.receiver_id && receiver_id == data.message.sender_id){
         const html = `
             <div style="margin-right: 20px;" class="flex items-center"  id="${data.message.id}-chat">
-                <span style="background: #efefef;" class="text-black text-justify rounded-md px-3 py-2">${data.message.message}</span>
+                <span style="background: #e5e7eb;" class="text-black text-justify rounded-md px-3 py-2">${data.message.message}</span>
             </div>
         `;
         $("#show-message").append(html);
