0c96e9139da763956df23f4d60d73dcfec0fe001
AshikSazal <sojolislm@gmail.com>
2024-08-15 01:02:46 +0600

    edit and delete menu add in chat.js file

--- public/src/js/chat.js
+++ public/src/js/chat.js
@@ -15,6 +15,21 @@ function showModification(){
             $(this).find('.show-modify-icon').removeClass("block").addClass("hidden");
         }
     );
+
+    $(".show-modify-icon").on('click', function(event) {
+        event.stopPropagation(); // Prevent the click event from bubbling up to the document
+        let chatId = $(this).data('id');
+        $(`.chat-modify-menu[data-id="${chatId}"]`).toggleClass('hidden');
+    });
+    // Hide the menu when clicking outside of it
+    $(document).on('click', function(event) {
+        var $target = $(event.target);
+
+        // Check if the click is inside a chatbox or its menu
+        if (!$target.closest('.chat-modify-menu').length) {
+            $('.chat-modify-menu').addClass('hidden');
+        }
+    });
 }
 
 $(document).ready(function(){
@@ -34,7 +49,7 @@ $(document).ready(function(){
                         background = '#1ca3e4';
                         textColor = 'text-white';
                         justify = 'justify-end';
-                        margin="margin-left: 30px;"
+                        margin="margin-left: 20px;"
                         svg=`<svg height="20" width="20" class="text-sky_blue_color -ml-2 cursor-pointer hidden show-modify-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-id="${chats[i].id}">
                                 <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
                             </svg>`;
@@ -42,12 +57,13 @@ $(document).ready(function(){
                     html += `
                         <div style="${margin}" class="flex items-center ${justify} show-modify mb-2" id="${chats[i].id}-chat">
                             ${svg}
-                            <span style="background: ${background};" class="${textColor} text-justify rounded-md px-3 py-2">${chats[i].message}</span>
+                            <span style="background: ${background}; max-width:90%;" class="${textColor} text-justify rounded-md px-3 py-2">${chats[i].message}</span>
                             <div class="chat-modify-menu absolute right-0 top-full mt-2 w-32 bg-white border border-gray-300 rounded-lg shadow-lg hidden" data-id="${chats[i].id}">
                                 <ul class="list-none p-2">
                                     <li>
                                         <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Edit</a>
                                     </li>
+                                    <li><hr></li>
                                     <li>
                                         <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Delete</a>
                                     </li>
@@ -59,12 +75,7 @@ $(document).ready(function(){
                 $("#show-message").append(html);
                 scrollChat();
                 showModification();
-                $(".show-modify-icon").on('click', function(event) {
-                    console.log("Hello");
-                    event.stopPropagation(); // Prevent the click event from bubbling up to the document
-                    let chatId = $(this).data('id');
-                    $(`.chat-modify-menu[data-id="${chatId}"]`).toggleClass('hidden');
-                });
+                
             },
             error: function(xhr, status, error){
                 const showError = document.getElementById('open-pop-up');
@@ -106,7 +117,18 @@ $(document).ready(function(){
                         <svg height="20" width="20" class="text-sky_blue_color -ml-2 cursor-pointer show-modify-icon hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" data-id="26">
                             <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"></path>
                         </svg>
-                        <span style="background: #e5e7eb;" class="text-white text-justify rounded-md px-3 py-2">${chat.message}</span>
+                        <span style="background: #e5e7eb; max-width:90%;" class="text-white text-justify rounded-md px-3 py-2">${chat.message}</span>
+                        <div class="chat-modify-menu absolute right-0 top-full mt-2 w-32 bg-white border border-gray-300 rounded-lg shadow-lg hidden" data-id="${chats[i].id}">
+                            <ul class="list-none p-2">
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Edit</a>
+                                </li>
+                                <li><hr></li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Delete</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 `;
                 $("#show-message").append(html);
