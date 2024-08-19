c51c78a7e60e54af59d6621377e8ad12edd9e545
AshikSazal <sojolislm@gmail.com>
2024-08-19 23:44:48 +0600

    chat delete and upate done

--- resources/views/features/chat/chat.blade.php
+++ resources/views/features/chat/chat.blade.php
@@ -160,14 +160,14 @@
             }
         });
 
-        chatSystem.style.display = 'none';
-        showChatBox.style.display = "flex";
-        showChatBox.style.overflow = "hidden";
-        showChatBox.style.width = "0";
-        showChatBox.style.height = "0";
-        showChatBox.style.width = "300px";
-        showChatBox.style.height = "400px";
-        messageSend.style.display = "flex";
+        // chatSystem.style.display = 'none';
+        // showChatBox.style.display = "flex";
+        // showChatBox.style.overflow = "hidden";
+        // showChatBox.style.width = "0";
+        // showChatBox.style.height = "0";
+        // showChatBox.style.width = "300px";
+        // showChatBox.style.height = "400px";
+        // messageSend.style.display = "flex";
 
         if (directMessage) {
             directMessage.addEventListener("click", function() {
