b96348529bc2031229d991bb8576fbe504e97ef2
AshikSazal <sojolislm@gmail.com>
2024-09-12 11:19:55 +0600

    fixing css in search file and modification in fetch process

--- resources/views/pages/brand-section/search.blade.php
+++ resources/views/pages/brand-section/search.blade.php
@@ -22,11 +22,16 @@
         </form>
     </div>
     <div class="sm:hidden block col-span-1 relative rounded-full w-full">
-        <input id="search-field" type="text" name="search" value="{{request('search')}}" placeholder="Search" class="border-[3px] rounded-lg top-10 absolute focus:outline-none border-orange_color block -left-40 p-2.5 w-[320px] hidden" />
+        <form action="" id="search-form2" class="">
+            <input id="search-field" type="text" name="search" value="{{request('search')}}" placeholder="Search" class="border-[3px] rounded-lg top-10 absolute focus:outline-none border-orange_color right-0 p-2.5 md:w-[320px] w-[250px] hidden" />
+        </form>
         <button id="search-field-open" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-white px-2 py-2 rounded">
             <svg id="search-open-icon" fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-6 h-6"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
             <svg id="search-close-icon" fill="#fff" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 hidden" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
         </button>
+        <div style="scrollbar-width: none;" class="absolute bg-gray-300 md:w-[320px] w-[250px] rounded-md overflow-hidden mt-24 right-0 max-h-48 overflow-y-auto" id="search-product-result2">
+            <ul id=""></ul>
+        </div>
     </div>
     <x-error />
 </div>
@@ -93,11 +98,7 @@
             return Math.round((1 - distance / maxLen) * 100);
         }
 
-        // Function to search products
-        $('#search-form').on('keyup change', 'input', function() {
-            const search = $("#search-product").val().trim();
-            const resultContainer = $("#search-product-result");
-
+        function showProduct(search, resultContainer){
             // Clear previous results
             resultContainer.find('ul').remove();
 
@@ -131,7 +132,6 @@
                 return acc.concat(products);
             }, []);
 
-            
             if (sortedProducts.length > 0) {
                 sortedProducts.forEach(product => {
                     const li = `<a href='#'><li class='p-2 hover:bg-orange_color hover:text-white hover:underline'>${product.name}</li></a><hr class='text-sky_blue_color'>`;
@@ -140,7 +140,20 @@
             } else {
                 ul.append(`<li class='p-2'>No results found</li>`);
             }
+        }
+
+        // Function to search products
+        $('#search-form').on('keyup change', 'input', function() {
+            const search = $("#search-product").val().trim();
+            const resultContainer = $("#search-product-result");
+            showProduct(search, resultContainer);
         });
+
+        $("#search-field").on('input',function(){
+            const search = $(this).val().trim();
+            const resultContainer = $("#search-product-result2");
+            showProduct(search, resultContainer);
+        })
 
         let searchOpen = $("#search-field").is(":visible");
         $("#search-field-open").on('click', function() {
