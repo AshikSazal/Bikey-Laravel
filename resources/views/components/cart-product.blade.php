0d8c38e887a0e3d1828b0d9b200383109c3d1091
AshikSazal <sojolislm@gmail.com>
2024-09-22 23:50:01 +0600

    fixing the cart product design

--- resources/views/components/cart-product.blade.php
+++ resources/views/components/cart-product.blade.php
@@ -9,8 +9,12 @@
                     <h1 class="font-bold text-2xl">{{ $cart['item']->name }}</h1>
                 </div>
                 <div class="row-span-1 flex items-end">
-                    <div class="flex space-x-10">
-                        <p>Quantity: {{ $cart['qty'] }}</p>
+                    <div class="flex space-x-10 items-center">
+                        <div class="flex items-center">
+                            <button id="decrease" class="p-2 rounded focus:outline-none text-2xl">-</button>
+                            <input style="-webkit-appearance: none; margin: 0; -moz-appearance: textfield;" type="number" value="{{ $cart['qty'] }}" class="w-10 text-center border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200" />
+                            <button id="increase" class="p-2 rounded focus:outline-none text-2xl">+</button>
+                        </div>
                         <p>${{ $cart['price'] }}</p>
                     </div>
                 </div>
