74a3842f7d88bea03f8340e3b5d4cbeb35b00f78
AshikSazal <sojolislm@gmail.com>
2024-10-22 00:18:10 +0600

    creating the landing page of admin panel

--- resources/views/admin-app.blade.php
+++ resources/views/admin-app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="./images/logo.png" type="image/png">
     <link rel="stylesheet" href="./src/css/loading.css">
-    {{-- <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script type="text/javascript" src="{{ URL::to('src/js/firebase.js') }}"></script>
     <script type="text/javascript" src="{{ URL::to('src/js/jquery.js') }}"></script>
     @vite(['resources/css/app.css', 'resources/css/header.css', 'resources/css/chat.css', 'resources/css/landing-page.css', 'resources/css/input.css','resources/js/app.js'])
@@ -30,17 +30,36 @@
 
     <div class="flex">
         <!-- Sidebar -->
-        <aside class="w-64 bg-white shadow-md h-screen">
+        <aside class="w-64 bg-white shadow-md h-screen relative">
             <div class="p-6">
                 <h1 class="text-2xl font-bold">Admin Panel</h1>
                 <nav class="mt-6">
                     <ul>
-                        <li><a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Dashboard</a></li>
-                        <li><a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Users</a></li>
-                        <li><a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Settings</a></li>
-                        <li><a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Reports</a></li>
+                        <li>
+                            <a href="#" class="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-200">
+                                <i class="fas fa-tachometer-alt mr-2"></i> Dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-200">
+                                <i class="fas fa-users mr-2"></i> Users
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-200">
+                                <i class="fas fa-cog mr-2"></i> Settings
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="flex items-center py-2 px-4 text-gray-700 hover:bg-gray-200">
+                                <i class="fas fa-chart-line mr-2"></i> Reports
+                            </a>
+                        </li>
                     </ul>
                 </nav>
+            </div>
+            <div class="absolute bottom-0 left-0 w-full p-6">
+                <a href="#" class="block w-full text-center bg-blue-600 text-white py-2 rounded hover:bg-blue-500">Login</a>
             </div>
         </aside>
 
@@ -49,6 +68,9 @@
             @yield('content')
         </main>
     </div>
+
+
+
 
     <script>
         // until fully loaded the page loading spinner will show
